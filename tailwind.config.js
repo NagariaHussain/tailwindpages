0380397b98f1ae809a6948c6b38aa5b83541eb03
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-02-13 15:10:41 +0530

    feat: everything works!

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./tailwindpages/www/**/*.html",
+    "./tailwindpages/templates/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
